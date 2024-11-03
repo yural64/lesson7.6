f712975e2a315abbdaaef6e26a694dc584417e53
yural64 <yural64@gmail.com>
2024-11-03 22:20:58 +0300

    add class

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from  selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+
+url = "https://www.divan.ru/kaluga/category/podvesnye-svetilniki"
+
+driver.get(url)
+time.sleep(3)
+lamps = driver.find_elements(By.CLASS_NAME, 'WdR1o')
