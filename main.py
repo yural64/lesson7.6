d5e12ad209bc291ec9d749592a4656c6a6a505c7
yural64 <yural64@gmail.com>
2024-11-03 21:50:34 +0300

    delete old code

--- main.py
+++ main.py
@@ -4,18 +4,3 @@ from  selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-
-class LampparsSpider(scrapy.Spider):
-    name = "lamppars"
-    allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://www.divan.ru/kaluga/category/svet"]
-
-    def parse(self, response):
-        lamps = response.css('div._Ud0k')
-        for lamp in lamps:
-            # Возвращаем информацию по каждой карточке
-            yield {
-                'name': lamp.css('div.lsooF span::text').get(),
-                'price': lamp.css('div.pY3d2 span::text').get(),  # получаем цену
-                'url': lamp.css('a').attrib['href']  # получаем ссылку
-            }
